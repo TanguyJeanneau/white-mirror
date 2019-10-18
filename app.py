49b3d0ca4bd2c184f106abbda0b81e32f5850e20
Tanguy Jeanneau <tanguy.jeanneau@gmail.com>
2019-10-18 23:25:20 +0200

    Upgraded API #8

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 #!../v2/v2venv/bin/python
-from flask import Flask, request, abort
+from flask import Flask, request, abort, Response, make_response, send_file
 import torch
+import cv2
+import json
+import jsonpickle
+import io
 
 from transfer import *
 from demo_images import load_image_as_tensor, style_transfer
+from main import reformat
 
 app = Flask(__name__)
 
@@ -41,8 +46,11 @@ def test():
 
 @app.route('/img')
 def img():
+    name_id = 'x'
     if 'img_path' not in request.args:
         return abort(404)
+    if 'id' in request.args:
+        name_id = request.args['id']
     img_path = request.args['img_path']
     frame = load_image_as_tensor(img_path, scale=2)
     if t.gpu:
@@ -50,15 +58,31 @@ def img():
     # get processed image
     output = style_transfer(frame, t)
     output = (1 + output)/2
-    save_image(output, '../imfolder/results/fromapi_out.jpg')
-    save_image(frame, '../imfolder/results/fromapi_in.jpg')
+    save_image(output, '../imfolder/results/{}_fromapi_out.jpg'.format(name_id))
+    save_image(frame, '../imfolder/results/{}_fromapi_in.jpg'.format(name_id))
     return img_path
 
 
-@app.route('/imglive')
+@app.route('/imglive', methods=['POST'])
 def imglive():
-    # WIP
-    return False
+    h = int(request.args['h'])
+    w = int(request.args['w'])
+    r = request
+    nparr = np.fromstring(r.data, np.uint8)
+    img = nparr.reshape([h, w, 3])
+    print(type(img), img.shape, img.min(), img.max())
+
+    output = torch.from_numpy(np.array([img]))
+    output = output.type('torch.FloatTensor')
+    output = output/255
+    output = torch.transpose(output, 1,3)
+    output = torch.transpose(output, 3,2)
+    output = style_transfer(output, t)
+    output = reformat(output)
+    print(type(output), output.shape, output.min(), output.max())
+
+    response = make_response(output.tostring())
+    return response
 
 
 if __name__ == '__main__':
