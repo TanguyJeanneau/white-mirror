8d74acb28771d1147c528e8d34eab4f171a94bc7
Tanguy Jeanneau <tanguy.jeanneau@gmail.com>
2020-02-11 09:06:07 +0100

    updated testapp.py using bytes encoding + changing endpoint

--- testapp.py
+++ testapp.py
@@ -15,18 +15,18 @@ Need to give a pretrained model (model_path variable)
 """
 
 if __name__ == '__main__':
-    post_url = 'http://127.0.0.1:5000/imglive'
-    # model_path = 'models/state_dict_STARWORKING_contentandstyle.pth'
-    model_path = 'models/state_dict_WAVEWORKING_stylecontent.pth'
-    t =  Transfer(10,
-                  './video/',
-                  './examples/style_img/wave.png',
-                  '/home/tfm/.torch/models/vgg19-dcbb9e9d.pth',
-                  1e-3,
-                  1e5, 1e7, 0, 1e-8)
+    post_url = 'http://127.0.0.1:8080/imglive'
+    # # model_path = 'models/state_dict_STARWORKING_contentandstyle.pth'
+    # model_path = 'models/state_dict_WAVEWORKING_stylecontent.pth'
+    # t =  Transfer(10,
+    #               './video/',
+    #               './examples/style_img/wave.png',
+    #               '/home/tfm/.torch/models/vgg19-dcbb9e9d.pth',
+    #               1e-3,
+    #               1e5, 1e7, 0, 1e-8)
     # loading model
-    print('loading state_dict')
-    t.style_net.load_state_dict(torch.load(model_path,  map_location='cpu'))
+    # print('loading state_dict')
+    # t.style_net.load_state_dict(torch.load(model_path,  map_location='cpu'))
     # some params
     width = 640
     height = 360
@@ -43,30 +43,18 @@ if __name__ == '__main__':
         # get cam image
         print('frame {}'.format(count))
         check, frame = video.read()
-
-        # HERE, prepare the image and give it to the API.
-        # THEN get the processed image and display it
-
-        # get processed image
-        # output = torch.from_numpy(np.array([frame]))
-        # output = output.type('torch.FloatTensor')
-        # output = output/255
-        # output = torch.transpose(output, 1,3)
-        # output = torch.transpose(output, 3,2)
-        # output = style_transfer(output, t)
-        # 
-        # # formating
-        # output = reformat(output)
         h, w, _ = frame.shape
-        print(type(frame), frame.shape, frame.min(), frame.max())
+        print(frame)
 
+        print('launching request')
         t1 = time.time()
         # h and w are quick fix for undesired image unfolding. To be changed
-        req = requests.post(post_url+'?h={}&w={}'.format(h, w), data=frame.tostring())
+        req = requests.post(post_url+'?h={}&w={}'.format(h, w), data=frame.tobytes())
         t2 = time.time()
         print('REQUETE EFFECTUEE EN {:.2f} s'.format(t2-t1))
 
-        nparr = np.fromstring(req.content, np.uint8)
+        # nparr = np.fromstring(req.content, np.uint8)
+        nparr = np.frombuffer(req.content, dtype=np.uint8)
         # VERY dirty, that's just temporary
         try:
             output = nparr.reshape([h+2, w, 3])
