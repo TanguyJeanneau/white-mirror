ae696a87595e92b050d7396591e57b79d9d58212
Tanguy Jeanneau <tanguy.jeanneau@gmail.com>
2020-02-11 08:41:39 +0100

    updated utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ from torchvision import transforms
 from configuration import *
 import cv2
 
+import io
+import zlib
+from flask import Flask, request, Response
+
 
 def reformat(img, float_to_int=True):
     """ from a pytorch tensor of shape b, c, h, w
@@ -24,3 +28,20 @@ def print_memory_usage():
                                      torch.cuda.max_memory_cached(0)))
     print('{0:.3e} / {1:.3e}'.format(torch.cuda.memory_allocated(0),
                                      torch.cuda.max_memory_allocated(0)))
+
+def compress_nparr(nparr):
+    """
+    https://gist.github.com/andres-fr/f9c0d5993d7e7b36e838744291c26dde
+    Returns the given numpy array as compressed bytestring,
+    the uncompressed and the compressed byte size.
+    """
+    bytestream = io.BytesIO()
+    np.save(bytestream, nparr)
+    uncompressed = bytestream.getvalue()
+    compressed = zlib.compress(uncompressed)
+    return compressed, len(uncompressed), len(compressed)
+
+def uncompress_nparr(bytestring):
+    """
+    """
+    return np.load(io.BytesIO(zlib.decompress(bytestring)))
